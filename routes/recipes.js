717086e0a2844efa0a41414c560dc66971361520
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-16 10:57:03 -0800

    reverted to working MVP

--- routes/recipes.js
+++ routes/recipes.js
@@ -9,7 +9,7 @@ router.get('/new', recipeCtrl.new)
 router.get('/:id', recipeCtrl.show)
 router.get('/:id/edit', recipeCtrl.edit)
 router.post("/", recipeCtrl.create)
-router.post("/:id/comments", recipeCtrl.createComment)
+router.post("/:id/comment", recipeCtrl.createComment)
 router.put("/:id", recipeCtrl.update)
 router.delete("/:id",recipeCtrl.delete)
 
